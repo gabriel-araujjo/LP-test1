7e81b148ddb7e55144531f6f87f75a99debea79b
Gabriel Araujo <gabriel140492@gmail.com>
2015-10-13 21:48:32 -0300

    identifying recursions type

--- src/search.h
+++ src/search.h
@@ -10,12 +10,14 @@
 
 /**
  * Return the position of element T
+ *
  */
 template<class T>
 int rSequentialSearch(T *vector, int size, T &element) {
     if (size == 0) return -1;
     if (*vector == element) return 0;
 
+    /* Recursão simples, não é de calda */
     int resultOfNextCall = rSequentialSearch(vector + 1, size - 1, element);
 
     return resultOfNextCall >= 0 ? 1 + resultOfNextCall : -1;
@@ -33,6 +35,9 @@ int rBinarySearch(T *vector, int size, T &element) {
 
     if (*candidate == element) return middle;
 
+    /* Recursão simples, apesar de haver duas chamadas a rBinarySearch, somente uma delas é chamada por vez,
+     * por isso não se trata de recursividade de calda
+     **/
     int resultOfNextCall;
     if (element < *candidate) {
         resultOfNextCall = rBinarySearch(vector, middle, element);
